89c8159fba2dc42028ca2d0a62ee712150659625
Md. Mokammel Hossen Farnan <Farnan130117@users.noreply.github.com>
2020-06-17 23:32:52 +0600

    Update views.py

--- store/views.py
+++ store/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+# FBV for store function
 def store(request):
 	context={}
 	return render(request,'store/store.html',context)
 
+# FBV for cart function
 def cart(request):
 	context={}
 	return render(request,'store/cart.html',context)
 
+# FBV for context function
 def checkout(request):
 	context={}
 	return render(request,'store/checkout.html',context)
